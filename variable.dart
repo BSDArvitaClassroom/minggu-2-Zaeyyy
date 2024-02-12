0a98ae13022922660e9e7e924a197e06f364bb46
ezaanom <ezaanom33@gmail.com>
2024-02-12 14:12:41 +0700

    Minggu 2

---
+++ variable.dart
@@ -0,0 +1,17 @@
+void main() {
+  var name = "eza"; // Tipe
+  var angka = 09;
+  var todayIsFriday = false;
+  print(name); // "eza"
+  print(angka); // 09
+  print(todayIsFriday); // false
+
+  var number = 19;
+  print(number < 22); // true
+  print(number > 19); // false
+  print(number >= 19); // true, karena terdapat sama dengan
+  print(number <= 22); // true
+
+  print(number = 10 + 9); // true
+  print(number = 27 - 8); // true
+}
